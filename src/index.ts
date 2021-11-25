9a02339868a57ff80c7ed4f301ed4af0e40ce203
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2021-11-25 19:20:40 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//ESDCRIBIR UNA FUNCION QUE RETORNE DOS PARAMETROS
+//por ejemplo si invocamos a la función con los parámetros 3 y a,
+//el resultado debería ser(3,a)===>[a,a,a]
+
+function retornoarreglo(numero: number, valor: any): any {
+  let exponentearreglo: any[] = new Array(numero);
+
+  for (let index = 0; index < exponentearreglo.length; index++) {
+    exponentearreglo[index] = valor;
+    console.log(exponentearreglo);
+  }
+  return retornoarreglo;
+}
+
+retornoarreglo(3, "b");
